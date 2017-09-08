88f41ba94cfdf60de87c21d4a95607b6111d78a2
Milly <milabits@gmail.com>
2017-09-08 11:56:18 +0200

    started work on cart grid code

---
+++ Cart RPG/Assets/Scripts/CartModuleGrid.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartModuleGrid : MonoBehaviour {
+    public Vector3 gridPosition;
+    public float gridLength;
+    public float gridWidth;
+    public int segments;
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gridPosition = transform.position + gridPosition;
+        
+    }
+
+    void LateUpdate()
+    {
+        Debug.DrawLine(
+            new Vector3(gridPosition.x - gridWidth / 2, gridPosition.y, gridPosition.z - gridLength / 2),
+            new Vector3(gridPosition.x - gridWidth / 2, gridPosition.y + 1, gridPosition.z - gridLength / 2), Color.red, 100);
+        Debug.DrawLine(
+            new Vector3(gridPosition.x + gridWidth / 2, gridPosition.y, gridPosition.z + gridLength / 2),
+            new Vector3(gridPosition.x + gridWidth / 2, gridPosition.y + 1, gridPosition.z + gridLength / 2), Color.red, 100);
+        Debug.DrawLine(
+            new Vector3(gridPosition.x + gridWidth / 2, gridPosition.y, gridPosition.z - gridLength / 2),
+            new Vector3(gridPosition.x + gridWidth / 2, gridPosition.y + 1, gridPosition.z - gridLength / 2), Color.red, 100);
+        Debug.DrawLine(
+            new Vector3(gridPosition.x - gridWidth / 2, gridPosition.y, gridPosition.z + gridLength / 2),
+            new Vector3(gridPosition.x - gridWidth / 2, gridPosition.y + 1, gridPosition.z + gridLength / 2), Color.red, 100);
+
+        Debug.DrawLine(
+            new Vector3(gridPosition.x - gridWidth / 2, gridPosition.y, gridPosition.z - gridLength / 2),
+            new Vector3(gridPosition.x + gridWidth / 2, gridPosition.y, gridPosition.z - gridLength / 2), Color.green, 100);
+        Debug.DrawLine(
+            new Vector3(gridPosition.x - gridWidth / 2, gridPosition.y, gridPosition.z - gridLength / 2),
+            new Vector3(gridPosition.x - gridWidth / 2, gridPosition.y, gridPosition.z + gridLength / 2), Color.green, 100);
+        Debug.DrawLine(
+            new Vector3(gridPosition.x - gridWidth / 2, gridPosition.y, gridPosition.z + gridLength / 2),
+            new Vector3(gridPosition.x + gridWidth / 2, gridPosition.y, gridPosition.z + gridLength / 2), Color.green, 100);
+        Debug.DrawLine(
+            new Vector3(gridPosition.x + gridWidth / 2, gridPosition.y, gridPosition.z + gridLength / 2),
+            new Vector3(gridPosition.x + gridWidth / 2, gridPosition.y, gridPosition.z - gridLength / 2), Color.green, 100);
+    }
+}
