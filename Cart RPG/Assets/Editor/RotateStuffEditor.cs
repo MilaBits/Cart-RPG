256e2f6000f8082d46836ea2d674a78322070cab
Milly <milabits@gmail.com>
2017-11-05 00:14:23 +0100

    Improved visuals on open world and added buildings

---
+++ Cart RPG/Assets/Editor/RotateStuffEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RotateStuff))]
+public class RotateStuffEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        RotateStuff rotateStuff = (RotateStuff)target;
+        if (GUILayout.Button("Rotate Objects")) {
+            rotateStuff.Rotate();
+        }
+    }
+}
---
+++ Cart RPG/Assets/Scripts/_Editor/RotateStuff.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RotateStuff : MonoBehaviour
+{
+    public List<GameObject> ObjectsToTurn;
+	
+    public void Rotate()
+    {
+        foreach (var gameObject in ObjectsToTurn) {
+            var euler = gameObject.transform.eulerAngles;
+            euler.z += Random.Range(0, 360);
+            gameObject.transform.eulerAngles = euler;
+        }
+    }
+}
